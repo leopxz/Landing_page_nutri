fd6b4d3f115a50b6c0f7267644e8d3f440e62819
leopxz <pxleonarddo@gmail.com>
2024-08-03 15:01:26 -0300

    Refactor: minificacao do css e ajustes no html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -13,9 +10,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -27,16 +21,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -54,9 +44,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+ 
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -72,9 +60,7 @@
     })
   }
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
+ 
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -88,9 +74,7 @@
     onscroll(document, headerScrolled)
   }
 
-  /**
-   * Back to top button
-   */
+ 
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -104,18 +88,14 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Mobile nav dropdowns activate
-   */
+ 
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -123,9 +103,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with offset on links with a class name .scrollto
-   */
+ 
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -141,9 +119,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with offset on page load with hash links in the url
-   */
+ 
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -152,9 +128,7 @@
     }
   });
 
-  /**
-   * Portfolio isotope and filter
-   */
+  
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -182,16 +156,12 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
+  
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Portfolio details slider
-   */
+ 
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -206,9 +176,7 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
+  
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -224,9 +192,7 @@
     }
   });
 
-  /**
-   * Animation on scroll
-   */
+ 
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
@@ -236,14 +202,10 @@
     })
   });
 
-  /**
-   * Initiate Pure Counter 
-   */
+ 
   new PureCounter();
 
-  /**
-   * Toggle Recipe Card
-   */
+
   window.toggleRecipeNew = function(header) {
     const card = header.closest('.recipe-card-new');
     card.classList.toggle('active');
